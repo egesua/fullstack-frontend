476adc4cec0abf904736fd500c0804c8b3173015
egesua <egesu06@gmail.com>
2024-03-24 14:56:21 +0300

    table upgrades and styling to new buttons.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
 
   const loadUsers = async () => {
     const result = await axios.get("http://localhost:8585/users");
-    console.log(result.data);
+    setUsers(result.data)
   };
 
   useEffect(() => {
@@ -20,23 +20,28 @@ const Home = () => {
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            {
-              users.map((user,index) => {
-                <tr>
-              <th scope="row" key={index}>{index + 1}</th>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-            </tr>
-              })
-            }
-            
+            {users.map((user, index) => {
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button className="btn btn-primary mx-2">View</button>
+                  <button className="btn btn-outline-primary mx-2">Edit</button>
+                  <button className="btn btn-danger mx-2">Delete</button>
+                </td>
+              </tr>;
+            })}
           </tbody>
         </table>
       </div>
