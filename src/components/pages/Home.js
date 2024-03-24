7e6487e8b528290296b29d7cb94d1b05c8545c47
egesua <egesu06@gmail.com>
2024-03-24 14:52:11 +0300

    mapping the users and index to make our table element dynamic.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,17 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Home = () => {
-
   const [users, setUsers] = useState([]);
 
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8585/users");
+    console.log(result.data);
+  };
+
   useEffect(() => {
-    console.log("egesua.")
-  },[])
+    loadUsers();
+  }, []);
 
   return (
     <div className="container">
@@ -21,23 +26,17 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+            {
+              users.map((user,index) => {
+                <tr>
+              <th scope="row" key={index}>{index + 1}</th>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
             </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+              })
+            }
+            
           </tbody>
         </table>
       </div>
