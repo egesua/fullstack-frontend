931547e64851b98a01551a6229fe866f838872ed
egesua <egesu06@gmail.com>
2024-03-24 14:32:46 +0300

    navbar added from bootstrap.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React from "react";
 
 const Navbar = () => {
   return (
     <div>
-      
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
     </div>
-  )
+  );
 };
 
 export default Navbar;
