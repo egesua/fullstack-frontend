27b5c898c31007c5be18cca7336a1291e3b908d1
egesua <egesu06@gmail.com>
2024-03-26 14:01:54 +0300

    addUser styling and build.

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -2,8 +2,23 @@ import React from "react"
 
 const AddUser = () => {
   return (
-    <div>
-      AddUser Form
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4"> Register User</h2>
+          <div className="mb-3">
+            <label htmlFor="Name" className="form-label">
+              Name
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your name"
+              name="name">
+            </input>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
